be36e440001c7dccf84277083f9d57da46c2d31c
Gaoyang Wang <33799914+Victor0829@users.noreply.github.com>
2018-04-19 00:19:56 -0700

    Update rbfm.cc

--- Project 1/rbf/rbfm.cc
+++ Project 1/rbf/rbfm.cc
@@ -16,6 +16,7 @@ RecordBasedFileManager::RecordBasedFileManager()
 
 RecordBasedFileManager::~RecordBasedFileManager()
 {
+    free(_rbf_manager);
 }
 
 RC RecordBasedFileManager::createFile(const string &fileName) {
@@ -39,7 +40,114 @@ RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
-    return -1;
+    const char* data_c = (char*)data;
+    uint16_t count = recordDescriptor.size();
+    uint16_t in_offset = ceil(recordDescriptor.size()/8.0);
+    uint16_t dir_offset = sizeof(uint16_t) + ceil(recordDescriptor.size()/8.0);
+    uint16_t data_offset = dir_offset + sizeof(uint16_t) * count;
+    uint16_t max_size = data_offset;
+    
+    for (int i = 0; i < count; ++i) {
+        max_size += recordDescriptor[i].length;
+    }
+    
+    char *record = (char*)calloc(max_size, sizeof(char));
+    memcpy(record, &count, sizeof(uint16_t));
+    memcpy(record + sizeof(uint16_t), &data_c[0], in_offset);
+    
+    for (int i = 0; i < count; ++i) {
+        char target = *(data_c + (char)(i/8));
+        if (!(target & (1<<(7-i%8)))) {
+            if (recordDescriptor[i].type == TypeVarChar) {
+                int attlen;
+                memcpy(&attlen, &data_c[in_offset], sizeof(int));     // Read length
+                memcpy(record + data_offset, &data_c[in_offset + 4], attlen);     // Write data
+                data_offset += attlen;
+                in_offset += (4 + attlen);
+                memcpy(record + dir_offset, &data_offset, sizeof(uint16_t));     // Write "dir entry"          
+            } 
+            
+            else {
+                if (recordDescriptor[i].type == TypeInt) {
+                    memcpy(record + data_offset, &data_c[in_offset], sizeof(int));
+                    in_offset += sizeof(int);
+                    data_offset += sizeof(int); 
+                    memcpy(record + dir_offset, &data_offset, sizeof(uint16_t));  
+                } 
+                
+                if (recordDescriptor[i].type == TypeReal) {
+                    memcpy(record + data_offset, &data_c[in_offset], sizeof(float));
+                    in_offset += sizeof(float);
+                    data_offset += sizeof(float); 
+                    memcpy(record + dir_offset, &data_offset, sizeof(uint16_t)); 
+                }
+            }
+            
+        } 
+        
+        else {
+            memcpy(record + dir_offset, &data_offset, sizeof(uint16_t));
+        }
+        dir_offset += sizeof(uint16_t);
+    } 
+     
+    char *page = (char*)calloc(PAGE_SIZE, sizeof(char)); 
+    uint16_t pageCount = fileHandle.getNumberOfPages(); 
+    uint16_t currPage = (pageCount>0) ? pageCount-1 : 0;
+    uint16_t freeSpace;
+    uint16_t recordCount;  
+    
+    char first = 1;
+    while (currPage < pageCount) {        
+        if (fileHandle.readPage(currPage, page) != 0)
+            return -1;
+        memcpy(&recordCount, &page[PAGE_SIZE - 4], sizeof(uint16_t));
+        memcpy(&freeSpace, &page[PAGE_SIZE - 2], sizeof(uint16_t));  
+
+        if (data_offset + 4 <= PAGE_SIZE - (freeSpace + 4 * recordCount + 4)) {
+            break;
+        }
+        if (!first) {
+            ++currPage;
+        } 
+        else {
+            currPage = 0;
+            first = 0;
+        }
+    }    
+    
+    // When we have no enough space on pages
+    if (currPage == pageCount) {
+        memset(page, 0, PAGE_SIZE); 
+        freeSpace = 0;
+        recordCount = 0;
+    }
+    recordCount++;
+
+    memcpy(page + freeSpace, record, data_offset);     // Actual records
+   
+    int page_dir = PAGE_SIZE - (4 * recordCount + 4);
+    memcpy(page + page_dir    , &freeSpace, sizeof(uint16_t));
+    memcpy(page + page_dir + 2, &data_offset, sizeof(uint16_t));
+    freeSpace += data_offset;  
+    memcpy(page + PAGE_SIZE - 4, &recordCount, sizeof(uint16_t));
+    memcpy(page + PAGE_SIZE - 2, &freeSpace, sizeof(uint16_t));    
+
+    int append_rc;
+    if (currPage == pageCount) {
+        append_rc = fileHandle.appendPage(page);
+    } 
+    else {
+        append_rc = fileHandle.writePage(currPage, page);
+        append_rc = fileHandle.readPage(currPage, page);
+    }
+    if (append_rc != 0) return -1;
+    
+    rid.slotNum = recordCount;
+    rid.pageNum = currPage;
+    free(record);
+    free(page);
+    return 0;
 }
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
